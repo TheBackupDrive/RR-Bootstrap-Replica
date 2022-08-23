90c97172970e50d60a14a6f8e4b5dbb3f04c2532
Phantom Daldust <phantom.daldust@gmail.com>
2022-08-22 21:18:34 -0400

    Everything is now styled

--- src/App.js
+++ src/App.js
@@ -11,20 +11,47 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 
-
 const Splash = styled.div`
   width: 100vw;
-  height: 100vh  ;
-  background-image: url(${background})
+  height: 50vh;
+  background-image: url(${background});
+  position: absolute;
+  left: 0;
+  right: 0;
 `
 
 const RedContainer = styled(Container)`
-  color: red;
+  color: darkred;
+  margin: auto;
 `
+const BlueContainer = styled(Container)`
+  color: darkblue;
+  margin: auto
+`
+
+const StylishContainer = styled(BlueContainer)`
+  font-size: 1.5em;
+  margin-top: 0.5em
+`
+
+const StyledRow = styled(Row)`
+  border-radius: 0.5em;
+  background-color: lightgreen;
+  padding: 10px
+`
+
+const StyledCol = styled(Col)`
+  border-radius: 0.5em;
+  background-color: seagreen;
+  padding: 10px;
+  text-align: center;
+  outline: 2px solid darkgreen
+`
+
 
 function App() {
   return (
-    <><Navbar bg={"light"} expand="lg" fixed = "top">
+    <><Navbar bg={"light"} expand="lg" sticky = "top">
       <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
         <img src={instaCartLogo} alt="logo" />
       </Navbar.Brand>
@@ -39,21 +66,33 @@ function App() {
     </Navbar>
     <Splash>
       <Container>
-        <Row>
-          <Col><h1>Welcome to the store</h1></Col>
-        </Row>
-        <Row>
-          <Col>Cool Stuff 1</Col>
-          <Col>Another Column</Col>
-          <Col>You guessed it, another Column</Col>
-        </Row>
+        <StyledRow>
+          <StyledCol><h1>Welcome to the store</h1></StyledCol>
+        </StyledRow>
+        <StyledRow>
+          <StyledCol>Cool Stuff 1</StyledCol>
+          <StyledCol>Another Column</StyledCol>
+          <StyledCol>You guessed it, another Column</StyledCol>
+        </StyledRow>
         <Row>
           <Col>
             <RedContainer>
-              <Row>
-                <Col>This should be red</Col>
-              </Row>
+              <StyledRow>
+                <StyledCol>Item 1</StyledCol>
+                <StyledCol>Item 2</StyledCol>
+                <StyledCol>Item 3</StyledCol>
+              </StyledRow>
             </RedContainer>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <StylishContainer>
+              <StyledRow>
+                <StyledCol>Column 1</StyledCol>
+                <StyledCol>Column 2</StyledCol>
+              </StyledRow>
+            </StylishContainer>
           </Col>
         </Row>
       </Container>
